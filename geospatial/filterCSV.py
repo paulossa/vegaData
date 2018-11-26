d9be2aa49954b3d409407f10ced92d7b77de44e1
Paulo Santos <paulo.araujo@ccc.ufcg.edu.br>
2018-11-26 11:34:45 -0300

    hopefully fixing files

--- geospatial/filterCSV.py
+++ geospatial/filterCSV.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 data = pd.read_csv("Basico_RJ.csv", sep=";", encoding="latin_1")
 print(data.shape)
+
 data2 = data.query("Nome_do_municipio == 'RIO DE JANEIRO'")
-
-
+print(data2.describe())
+data2 = data2.drop(columns=['Unnamed: 33'])
+print(data2.describe())
 data2.to_csv('filtered.csv', encoding='utf-8', index=False, index_label=False)
